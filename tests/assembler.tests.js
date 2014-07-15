f25de40cc09f749e6a888dbe8fe930d73b2c377e
Michael Williamson <mike@zwobble.org>
2014-07-15 17:23:41 +0100

    Support or assembly

--- tests/assembler.tests.js
+++ tests/assembler.tests.js
@@ -15,6 +15,7 @@ exports["mfhi"] = assembleLineTest("mfhi $t1", "00000000000000000100100000010000
 exports["mflo"] = assembleLineTest("mflo $t1", "00000000000000000100100000010010");
 
 exports["and"] = assembleLineTest("and $t1, $t2, $t3", "00000001010010110100100000100100");
+exports["or"] = assembleLineTest("or $t1, $t2, $t3", "00000001010010110100100000100101");
 
 // I-type
 exports["addi"] = assembleLineTest("addi $t1, $t2, 0x42", "00100001010010010000000001000010");
