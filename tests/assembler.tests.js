0504506e20ed81b02f981b9af4e3453098771112
Michael Williamson <mike@zwobble.org>
2014-07-15 17:04:24 +0100

    Add test for subu assembly

--- tests/assembler.tests.js
+++ tests/assembler.tests.js
@@ -4,6 +4,7 @@ var assembler = require("../lib/assembler");
 exports["add"] = assembleLineTest("add $t1, $t2, $t3", "1010010110100100000100000");
 exports["addu"] = assembleLineTest("addu $t1, $t2, $t3", "1010010110100100000100001");
 exports["sub"] = assembleLineTest("sub $t1, $t2, $t3", "00000001010010110100100000100010");
+exports["subu"] = assembleLineTest("subu $t1, $t2, $t3", "00000001010010110100100000100011");
 
 // I-type
 exports["addi"] = assembleLineTest("addi $t1, $t2, 0x42", "00100001010010010000000001000010");
