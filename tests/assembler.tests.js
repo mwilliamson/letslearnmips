81ad8876dba205b654f46b4287a4123f1d2c6c8f
Michael Williamson <mike@zwobble.org>
2014-07-15 17:05:28 +0100

    Implement addiu assembly

--- tests/assembler.tests.js
+++ tests/assembler.tests.js
@@ -5,9 +5,11 @@ exports["add"] = assembleLineTest("add $t1, $t2, $t3", "101001011010010000010000
 exports["addu"] = assembleLineTest("addu $t1, $t2, $t3", "1010010110100100000100001");
 exports["sub"] = assembleLineTest("sub $t1, $t2, $t3", "00000001010010110100100000100010");
 exports["subu"] = assembleLineTest("subu $t1, $t2, $t3", "00000001010010110100100000100011");
+exports["subu"] = assembleLineTest("subu $t1, $t2, $t3", "00000001010010110100100000100011");
 
 // I-type
 exports["addi"] = assembleLineTest("addi $t1, $t2, 0x42", "00100001010010010000000001000010");
+exports["addiu"] = assembleLineTest("addiu $t1, $t2, 0x42", "00100101010010010000000001000010");
 
 
 function assembleLineTest(line, expectedResult) {
