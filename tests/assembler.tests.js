5a39bb0ebe84013ffa4d60cecabc9f30055960e2
Michael Williamson <mike@zwobble.org>
2014-07-16 15:52:29 +0100

    Support lbu assembly

--- tests/assembler.tests.js
+++ tests/assembler.tests.js
@@ -38,6 +38,7 @@ exports["lw"] = assembleLineTest("lw $t1, 16($t2)", "100011010100100100000000000
 exports["lh"] = assembleLineTest("lh $t1, 16($t2)", "10000101010010010000000000010000");
 exports["lhu"] = assembleLineTest("lhu $t1, 16($t2)", "10010101010010010000000000010000");
 exports["lb"] = assembleLineTest("lb $t1, 16($t2)", "10000001010010010000000000010000");
+exports["lbu"] = assembleLineTest("lbu $t1, 16($t2)", "10010001010010010000000000010000");
 exports["lui"] = assembleLineTest("lui $t1, 0x42", "00111100000010010000000001000010");
 
 exports["andi"] = assembleLineTest("andi $t1, $t2, 0x42", "00110001010010010000000001000010");
